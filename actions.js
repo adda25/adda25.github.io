8b48c1c6f3e12d79b04878c2f06e622df62cd5f1
Amedeo Setti <amedeosetti@gmail.com>
2016-08-31 21:58:17 +0200

    Added test site

---
+++ actions.js
@@ -0,0 +1,24 @@
+function setTitle(title) {
+  var str = '<h1 class="page-header">'.concat(title).concat('</h1>')
+  var msg = str
+  document.getElementById('name').innerHTML = msg;
+}
+
+function setImages() {
+  var folder = "images/";
+
+  $.ajax({
+      url : folder,
+      success: function (data) {
+          $(data).find("a").attr("href", function (i, val) {
+              if( val.match(/\.(JPG|jpg|png|gif)$/) ) { 
+                  $("body").append( "<img src='"+ folder + val +"'>" );
+              } 
+          });
+          console.log(data);
+      }
+  });
+}
+
+
+
