ca6a9051cbcce9127560b0aba56f410e758c1b4b
Amedeo Setti <amedeosetti@gmail.com>
2016-09-06 21:33:11 +0200

    Working on form

--- actions.js
+++ actions.js
@@ -8,9 +8,13 @@ function setTitle(title) {
   document.getElementById('name').innerHTML = msg;
 }
 
+
+// \\\\\\\\\\\\\\\\\\\\\\\\\\\\
+// For carousel images handling
+// \\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
 function setImages() {
   var folder = "images/";
-
   $.ajax({
       url : folder,
       success: function (data) {
@@ -25,4 +29,57 @@ function setImages() {
 }
 
 
+// \\\\\\\\\\\\\\\\\\\\\\\\\\\\
+// Form handling
+// \\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
+$("input[name=submit]").click(function(e) {
+  var mess = $("textarea[name=message]").val();
+  var mail = $("input[name=mail]").val();
+  if (mess != "") {
+    $.post("http://asetti.altervista.org/respond.php",
+      {message: mess, mail: mail}).
+      done(function(d) { console.log(d);
+        if(d == "NO") {
+          alert("There was an error...");
+        }
+        if (d == "OK") {
+          alert("Message sent!");
+          $("textarea[name=message]").val("");
+          $("input[name=mail]").val("");
+        }
+      });
+  }
+  return false;
+})
+
+// \\\\\\\\\\\\\\\\\\\\\\\\\\\\
+// Scroll animation JS
+// \\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+var $animation_elements = $('.animation-element');
+var $window = $(window);
+
+function check_if_in_view() {
+  var window_height = $window.height();
+  var window_top_position = $window.scrollTop();
+  var window_bottom_position = (window_top_position + window_height);
+
+  $.each($animation_elements, function() {
+    var $element = $(this);
+    var element_height = $element.outerHeight();
+    var element_top_position = $element.offset().top;
+    var element_bottom_position = (element_top_position + element_height);
+
+    //check to see if this current container is within viewport
+    if ((element_bottom_position >= window_top_position) &&
+      (element_top_position <= window_bottom_position)) {
+      $element.addClass('in-view');
+    } else {
+      $element.removeClass('in-view');
+    }
+  });
+}
+
+$window.on('scroll resize', check_if_in_view);
+$window.trigger('scroll');
